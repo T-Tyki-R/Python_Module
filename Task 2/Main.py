749862ee1bddf02d3b814586db1bc85c86473cd3
Tameka Roberson <ttrobersoncreate@gmail.com>
2024-06-27 14:51:19 -0400

    Task 2 Completed

---
+++ Task 2/Main.py
@@ -0,0 +1,5 @@
+import Text_Utils as text_mod
+
+user_word = input("Hello user! Enter a word and I'll make some magic happen: ")
+print(text_mod.reverse_text(user_word))
+print(text_mod.captialize_text(user_word))
---
+++ Task 2/Text_Utils.py
@@ -0,0 +1,5 @@
+def reverse_text(word = ""):
+    return word[::-1]
+
+def captialize_text(word = ""):
+    return word.capitalize()
